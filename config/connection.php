5aba33ebaa21861c5f561f78f4fb289a65b1433c
EverKlett <everton.klettenberg@gmail.com>
2020-11-19 19:59:34 -0300

    Update Connect function
    Removed function params

--- config/connection.php
+++ config/connection.php
@@ -1,15 +1,14 @@
 <?php
-    $host = "localhost";
-    $database = "Northwind";
-    $user = "web";
-    $password = "123";
-
-    function Connect($host, $database, $user, $password) {
-        $connection = new PDO("sqlsrv:Server={$host};Database={$database};ConnectionPooling=0", "web", "123");
+    function Connect() {
+        $host = "localhost";
+        $database = "Northwind";
+        $user = "web";
+        $password = "123";
+        $connection = new PDO("sqlsrv:Server={$host};Database={$database};ConnectionPooling=0", $user, $password);
 
         return $connection;
     }
     
-    $connection = Connect($host, $database, $user, $password);
+    $connection = Connect();
     var_dump($connection);
 ?>
