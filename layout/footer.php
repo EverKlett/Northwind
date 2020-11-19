9ba7fa2868550bf1335fb73ee11f4ddb8a750f62
EverKlett <everton.klettenberg@gmail.com>
2020-11-19 20:41:26 -0300

    Initial Bootstrap
    Added nav

---
+++ layout/footer.php
@@ -0,0 +1,9 @@
+<?php
+    require_once 'config/parameter.php';
+    require_once LAYOUT."header.php";
+
+    echo "<p>Fooooooooooooootteeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrrrrrr</p>";
+?>
+    </div class="container">
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,9 +1,7 @@
 <?php
     require_once 'config/parameter.php';
     require_once LAYOUT."header.php";
-    require_once LAYOUT."menu.php";
 
-    echo "<p>Hello World</p>";
 
     require_once LAYOUT."footer.php";
 ?>
