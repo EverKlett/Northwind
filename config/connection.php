58647c7bdb149c69bb53c054adb1320e5d14552e
EverKlett <everton.klettenberg@gmail.com>
2020-11-19 19:41:35 -0300

    FIxed connection
    Fixed php - MSSQL Server connection

--- config/connection.php
+++ config/connection.php
@@ -1,11 +1,15 @@
 <?php
+    $host = "localhost";
+    $database = "Northwind";
+    $user = "web";
+    $password = "123";
 
-    function Connect() {
-        $connection = new PDO("sqlsrv:Server=locaohost;Database=Northwind", "web", "123");
+    function Connect($host, $database, $user, $password) {
+        $connection = new PDO("sqlsrv:Server={$host};Database={$database};ConnectionPooling=0", "web", "123");
 
         return $connection;
     }
     
-    $connection = Connect();
+    $connection = Connect($host, $database, $user, $password);
     var_dump($connection);
 ?>
